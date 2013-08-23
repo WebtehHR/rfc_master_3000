d77a22ff4ca6e52cd4f42d227e5c61e95b9d9fbb
Borna Novak <dosadnizub@gmail.com>
2013-08-23 18:25:47 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RfcMaster3000::Application.config.secret_key_base = '953018ecca1e02ee2e20e137237f9b97a95014139e12f8061ed7be191dcc60920af127e8584a856889b5fc86adbd9f14b957ebbb3848310465278116c46a4a96'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RfcMaster3000::Application.config.session_store :cookie_store, key: '_rfc_master_3000_session'
