16ffd302b3ae2ea221c5dc38a61d18a5476627a8
Borna Novak <dosadnizub@gmail.com>
2013-08-29 07:58:19 +0200

    Added people to RFC index

--- app/helpers/request_for_changes_helper.rb
+++ app/helpers/request_for_changes_helper.rb
@@ -1,2 +1,7 @@
 module RequestForChangesHelper
+
+  def pretty_dropdown_link_for_person rfc, person_symbol
+    link_to("#{person_symbol.to_s[0].upcase}: #{rfc.send("#{person_symbol}_name_with_description")}", '#', :class => 'btn align-left') if rfc.send(person_symbol)
+  end
+
 end
