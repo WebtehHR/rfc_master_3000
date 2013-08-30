241c5bba759a0d5ebc3a1d574311c5d2284599e8
Tomislav Adamic <tomislav.adamic@webteh.hr>
2013-08-30 11:37:55 +0200

    Adding Oracle database support

--- db/schema.rb
+++ db/schema.rb
@@ -16,48 +16,48 @@ ActiveRecord::Schema.define(version: 20130828054750) do
   create_table "request_for_changes", force: true do |t|
     t.string   "noc_tracking_url"
     t.string   "webteh_tracking_url"
-    t.boolean  "type_network"
-    t.boolean  "type_servers"
-    t.boolean  "type_application"
-    t.boolean  "type_user_management"
-    t.integer  "requestor_id"
+    t.boolean  "type_network",             precision: 1,  scale: 0
+    t.boolean  "type_servers",             precision: 1,  scale: 0
+    t.boolean  "type_application",         precision: 1,  scale: 0
+    t.boolean  "type_user_management",     precision: 1,  scale: 0
+    t.integer  "requestor_id",             precision: 38, scale: 0
     t.string   "description_of_change"
-    t.boolean  "change_repair"
-    t.boolean  "change_removal"
-    t.boolean  "change_emergency"
-    t.boolean  "change_other"
-    t.date     "request_implement_window"
+    t.boolean  "change_repair",            precision: 1,  scale: 0
+    t.boolean  "change_removal",           precision: 1,  scale: 0
+    t.boolean  "change_emergency",         precision: 1,  scale: 0
+    t.boolean  "change_other",             precision: 1,  scale: 0
+    t.datetime "request_implement_window"
     t.string   "systems_affected"
     t.string   "users_affected"
     t.string   "criticality_of_change"
     t.text     "test_plan"
     t.text     "back_out_plan"
-    t.integer  "management_approver_id"
+    t.integer  "management_approver_id",   precision: 38, scale: 0
     t.string   "mgmt_approval_status"
     t.string   "mgmt_approval_comments"
-    t.date     "change_scheduled_for"
-    t.date     "mgmt_decision_date"
-    t.integer  "security_approver_id"
+    t.datetime "change_scheduled_for"
+    t.datetime "mgmt_decision_date"
+    t.integer  "security_approver_id",     precision: 38, scale: 0
     t.string   "sec_approval_status"
     t.string   "sec_approval_comments"
-    t.date     "sec_decision_date"
-    t.integer  "implementor_id"
+    t.datetime "sec_decision_date"
+    t.integer  "implementor_id",           precision: 38, scale: 0
     t.string   "implementation_status"
     t.string   "implement_comments"
-    t.date     "implementation_start"
-    t.date     "implementation_end"
+    t.datetime "implementation_start"
+    t.datetime "implementation_end"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "type_documentation"
+    t.boolean  "type_documentation",       precision: 1,  scale: 0
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                                           default: "", null: false
+    t.string   "encrypted_password",                              default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
+    t.integer  "sign_in_count",          precision: 38, scale: 0, default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -69,22 +69,22 @@ ActiveRecord::Schema.define(version: 20130828054750) do
     t.string   "full_name"
     t.string   "title"
     t.string   "role"
-    t.boolean  "user_admin"
+    t.boolean  "user_admin",             precision: 1,  scale: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["reset_password_token"], name: "i_users_reset_password_token", unique: true
 
   create_table "versions", force: true do |t|
-    t.string   "item_type",      null: false
-    t.integer  "item_id",        null: false
-    t.string   "event",          null: false
+    t.string   "item_type",                               null: false
+    t.integer  "item_id",        precision: 38, scale: 0, null: false
+    t.string   "event",                                   null: false
     t.string   "whodunnit"
     t.text     "object"
     t.datetime "created_at"
     t.text     "object_changes"
   end
 
-  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
+  add_index "versions", ["item_type", "item_id"], name: "i_versions_item_type_item_id"
 
 end
